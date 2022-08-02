dad9522d99c4b609f024b18fad6eb48fd21dc006
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-02 20:55:22 -0300

    Revert "Revert "Criação das models Clientes, FormaDePagamentos, Usuarios e Veiculos""
    This reverts commit 076b303d08b08989af4bb51a9e40cbcfe6016303.

---
+++ CarLocadora.Modelo/Veiculos/VeiculosModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLocadora.Modelo.Veiculos
+{
+    public class VeiculosModel
+    {
+        [StringLength(8)]
+        public string Placa { get; set; }
+        [StringLength(100)]
+        public string? Chassi { get; set; }
+        [StringLength(100)]
+        public string Marca { get; set; }
+        [StringLength(150)]
+        public string Modelo { get; set; }
+        [StringLength(100)]
+        public string Combustivel { get; set; }
+        [StringLength(100)]
+        public string Cor { get; set; }
+        [StringLength(2000)]
+        public string? Opcionais { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
