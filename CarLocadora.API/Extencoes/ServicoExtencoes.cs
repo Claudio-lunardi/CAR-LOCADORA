23b8b1515cdf836eeeb4120f4e17358bc0b22159
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-02 21:44:36 -0300

    Criação do ServicoExtecoes

---
+++ CarLocadora.API/Extencoes/ServicoExtencoes.cs
@@ -0,0 +1,15 @@
+﻿using CarLocadora.Infra.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarLocadora.API.Extencoes
+{
+    public static class ServicoExtencoes
+    {
+        public static void ConfigurarServicos(this IServiceCollection services)
+        {
+            string connectionString = "Data Source=localhost,1434;User ID=sa;Password=senha@1234xxxY;Initial Catalog=DBCarLocadora;";
+            services.AddDbContext<Context>(item => item.UseSqlServer(connectionString));
+
+        }
+    }
+}
