90cbc1afca50df36cbbcb6d0499aa851f0172777
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-02 21:11:46 -0300

    Adicionando pasta Entity e Context

---
+++ CarLocadora.Infra/Entity/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLocadora.Infra.Entity
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
