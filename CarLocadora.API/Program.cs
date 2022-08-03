608543af343cd88f0b33143b0f3924709d3aa495
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-03 19:18:02 -0300

    Iniciando Code First

--- CarLocadora.API/Program.cs
+++ CarLocadora.API/Program.cs
@@ -1,5 +1,8 @@
+using CarLocadora.API.Extencoes;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigurarServicos();
 // Add services to the container.
 
 builder.Services.AddControllers();
--- CarLocadora.Infra/Entity/EntityContext.cs
+++ CarLocadora.Infra/Entity/EntityContext.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarLocadora.Infra.Entity
 {
-    public class EntityContext:DbContext
+    public class EntityContext : DbContext
     {
 
         public EntityContext(DbContextOptions<EntityContext> options) : base(options)
