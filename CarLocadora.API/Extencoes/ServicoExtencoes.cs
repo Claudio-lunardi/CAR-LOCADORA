ac70da9ff92ee129ef5c871e32a64d97a77d9ed4
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-03 21:06:17 -0300

    Code first

--- CarLocadora.API/Extencoes/ServicoExtencoes.cs
+++ CarLocadora.API/Extencoes/ServicoExtencoes.cs
@@ -8,6 +8,7 @@ namespace CarLocadora.API.Extencoes
         public static void ConfigurarServicos(this IServiceCollection services)
         {
             string connectionString = "Data Source=localhost,1434;User ID=sa;Password=senha@1234xxxY;Initial Catalog=DBCarLocadora;";
+
             services.AddDbContext<EntityContext>(item => item.UseSqlServer(connectionString));
 
         }
