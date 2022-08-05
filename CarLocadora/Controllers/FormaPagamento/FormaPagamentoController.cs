6b8daceb48643087a22495fcd481376e7fd19423
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-05 20:43:52 -0300

    Inclusão front-end FormaPagamento

---
+++ CarLocadora/Controllers/FormaPagamento/FormaPagamentoController.cs
@@ -0,0 +1,151 @@
+﻿using CarLocadora.Modelo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace CarLocadora.Controllers.FormaPagamento
+{
+    public class FormaPagamentoController : Controller
+    {
+        // GET: FormaPagamentoController
+        #region Index
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                HttpClient Cliente = new HttpClient();
+
+                Cliente.DefaultRequestHeaders.Accept.Clear();
+                Cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = Cliente.GetAsync("https://localhost:7142/api/CadastroFormaPagamento").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    return View(JsonConvert.DeserializeObject<List<FormasDePagamentosModel>>(json));
+                }
+                else
+                {
+                    throw new Exception("aaa");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        // GET: FormaPagamentoController/Details/5
+        public ActionResult Details(int valor)
+        {
+            HttpClient Cliente = new HttpClient();
+            Cliente.DefaultRequestHeaders.Accept.Clear();
+            Cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = Cliente.GetAsync($"https://localhost:7142/api/CadastroFormaPagamento/ObterUmaFormaPagamento?valor={valor}").Result;
+
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                return View(JsonConvert.DeserializeObject<FormasDePagamentosModel>(conteudo));
+            }
+            else
+            {
+                throw new Exception("aaa");
+
+            }
+        }
+
+        // GET: FormaPagamentoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FormaPagamentoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromForm] FormasDePagamentosModel formasDePagamentosModel)
+        {
+            HttpClient Cliente = new HttpClient();
+
+            Cliente.DefaultRequestHeaders.Accept.Clear();
+            Cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = Cliente.PostAsJsonAsync("https://localhost:7142/api/CadastroFormaPagamento", formasDePagamentosModel).Result;
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                throw new Exception("aaa");
+            }
+
+
+        }
+
+        // GET: FormaPagamentoController/Edit/5
+        public ActionResult Edit(int valor)
+        {
+            HttpClient Cliente = new HttpClient();
+            Cliente.DefaultRequestHeaders.Accept.Clear();
+            Cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = Cliente.GetAsync($"https://localhost:7142/api/CadastroFormaPagamento/ObterUmaFormaPagamento?valor={valor}").Result;
+
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                return View(JsonConvert.DeserializeObject<FormasDePagamentosModel>(conteudo));
+            }
+            else
+            {
+                throw new Exception("aaa");
+
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([FromForm]FormasDePagamentosModel formasDePagamentosModel)
+        {
+            try
+            {
+                HttpClient Cliente = new HttpClient();
+                Cliente.DefaultRequestHeaders.Accept.Clear();
+                Cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = Cliente.PutAsJsonAsync("https://localhost:7142/api/CadastroCliente", formasDePagamentosModel).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    throw new Exception("aaa");
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+        }
+
+    }
+}
