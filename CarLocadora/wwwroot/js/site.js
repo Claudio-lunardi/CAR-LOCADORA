e55ee2c8b874609e4f14abb37fb8e05736e12a6e
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-08-07 12:39:39 -0300

    Modal Excluir

--- CarLocadora/wwwroot/js/site.js
+++ CarLocadora/wwwroot/js/site.js
@@ -2,8 +2,12 @@
 
     $('.maskTelefone').inputmask({ mask: ['(99) 9999-99999'] });
     $('.maskCelular').inputmask({ mask: ['(99) 99999-9999'] });
+    $('.maskPlaca').inputmask({ mask: ['(99) 99999-9999'] });
 
     $('.maskCPF').inputmask({ mask: ['999.999.999-99'] });
     $('.maskCNPJ').inputmask({ mask: ['99.999.999/9999-99'] });
-     
+
+
+
+
 })
