82fef207757b3912c694316cfe379b6cb270adb2
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-10-02 23:27:13 -0300

    Tarefa email

--- CarLocadora.EnviarEmail/Worker.cs
+++ CarLocadora.EnviarEmail/Worker.cs
@@ -22,70 +22,67 @@ namespace CarLocadora.EnviarEmail
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
-
             MailMessage mail = new MailMessage();
-
             mail.From = new MailAddress("turma1@devpratica.com.br");
+
+            SmtpClient client = new SmtpClient("smtp.kinghost.net");
+            client.Port = 587;
+            client.EnableSsl = false;
+            client.Credentials = new NetworkCredential("turma1@devpratica.com.br", "Turma1@1");
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                var EmailCliente = await GetEmailCliente();
+                var EmailCliente = await BuscarEmailCliente();
 
                 if (EmailCliente.Any(x => x.emailEnviado == false))
-                {
+{
                     foreach (var item in EmailCliente)
                     {
                         mail.To.Add(item.Email);
                     }
 
-                    mail.Subject = "aa";
-                    mail.Body = "av";
-
-                    SmtpClient client = new SmtpClient("smtp.kinghost.net");
-                    client.Port = 587;
-                    client.EnableSsl = false;
-                    client.Credentials = new NetworkCredential("turma1@devpratica.com.br", "Turma1@1");
+                    mail.Subject = "texto fixo";
+                    mail.Body = "teste";
 
                     for (int i = 0; i < EmailCliente.Count; i++)
                     {
-                        await EmailTrue(EmailCliente[i]);
+                        await EditarCampoEmailEnviado(EmailCliente[i]);
                     }
 
                     client.Send(mail);
                 }
-           
+
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
 
 
-        private async Task<List<ClientesModel>> GetEmailCliente()
+        private async Task<List<ClientesModel>> BuscarEmailCliente()
         {
             HttpResponseMessage r = await _httpClient.GetAsync("https://localhost:7142/api/CadastroCliente/ObterListaEnviarEmail");
             if (r.IsSuccessStatusCode)
             {
-                var re = JsonConvert.DeserializeObject<List<ClientesModel>>(await r.Content.ReadAsStringAsync());
-                return re;
+                return JsonConvert.DeserializeObject<List<ClientesModel>>(await r.Content.ReadAsStringAsync());
             }
             else
             {
-                return null;
+                throw new Exception(r.ReasonPhrase);
             }
-
-
-
 
         }
 
-        private async Task EmailTrue(ClientesModel clientesModel)
+        private async Task EditarCampoEmailEnviado(ClientesModel clientesModel)
         {
             clientesModel.emailEnviado = true;
             HttpResponseMessage r = await _httpClient.PutAsJsonAsync($"https://localhost:7142/api/CadastroCliente", clientesModel);
             if (r.IsSuccessStatusCode)
             {
 
+            }
+            else
+            {
+                throw new Exception(r.ReasonPhrase);
             }
         }
 
