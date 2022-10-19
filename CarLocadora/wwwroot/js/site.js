639a15beeb8da4794a9818ed105f814ec9e7968e
Claudio-lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2022-10-19 15:29:56 -0300

    Ajuste layout

--- CarLocadora/wwwroot/js/site.js
+++ CarLocadora/wwwroot/js/site.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function () {
-
+    //MARCARA DOS CAMPOS
     $('.maskTelefone').inputmask({ mask: ['(99) 9999-99999'] });
     $('.maskCelular').inputmask({ mask: ['(99) 99999-9999'] });
     $('.maskPlaca').inputmask({ mask: ['AAA9*99'] });
     $('.maskCPF').inputmask({ mask: ['999.999.999-99'] });
     $('.maskCNPJ').inputmask({ mask: ['99.999.999/9999-99'] });
 
-    //CONFIGURAÇÃO GRID
+    //CONFIGURAÇÃO DO GRID
     $('#myTable').DataTable({
         "language": {
             "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
@@ -14,9 +14,9 @@
         "lengthMenu": [10, 15, 20, 30, 100]
     });
 
+    //NÃO ALTERAR SE NÃO OUVER EDIÇÃO
     var inicial = $("#Form").serialize();
     $("#Form").on("submit", function (e) {
-
         var atual = $("#Form").serialize();
 
         if (atual !== inicial) {
